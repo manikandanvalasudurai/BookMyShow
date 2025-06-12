0175069cc3060aa30e6dfc7cae90aebd73619cfd
Manikandan <manikandanvalasudurai@gmail.com>
2025-06-12 15:44:45 +0530

    Login and Signup finished

--- src/main/java/com/mani/example/bookmyshowjun25/BookMyShowJun25Application.java
+++ src/main/java/com/mani/example/bookmyshowjun25/BookMyShowJun25Application.java
@@ -2,8 +2,10 @@ package com.mani.example.bookmyshowjun25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookMyShowJun25Application {
 
     public static void main(String[] args) {
--- src/main/java/com/mani/example/bookmyshowjun25/models/BaseModel.java
+++ src/main/java/com/mani/example/bookmyshowjun25/models/BaseModel.java
@@ -1,16 +1,21 @@
 package com.mani.example.bookmyshowjun25.models;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.util.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @CreatedDate
+    @Temporal(TemporalType.DATE) //By default TemporalType should be Milliseconds
     private Date createdAt;
+    @LastModifiedDate
     private Date lastModifiedAt;
 
     public Long getId() {
@@ -37,4 +42,9 @@ public class BaseModel {
         this.lastModifiedAt = lastModifiedAt;
     }
 }
+/*
+1.Enable @EnableJpaAuditing on the Application level
+2.EntityListeners on the BaseModel class
+3.@CreatedData and @LastModifiedDate on Attribute level
+ */
 
